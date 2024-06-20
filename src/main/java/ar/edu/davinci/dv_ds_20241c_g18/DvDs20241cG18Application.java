16f9b873be1ec12ed4f63f1dba6a3e9a0c00d287
Alexander montes de oca <alexandermontesdeoca@Alexanders-MacBook-Pro.local>
2024-06-20 13:01:33 -0300

    Configuración inicial del proyecto Spring Boot

---
+++ src/main/java/ar/edu/davinci/dv_ds_20241c_g18/DvDs20241cG18Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20241c_g18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20241cG18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20241cG18Application.class, args);
+	}
+
+}
