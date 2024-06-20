2806e90cdaa90af4d47ff3f42fcac046b1b8f2a5
Alexander montes de oca <alexandermontesdeoca@Alexanders-MacBook-Pro.local>
2024-06-20 12:12:06 -0300

    Init project

---
+++ src/test/java/dv_ds_20241c_g18/tiendaRopita/AppTest.java
@@ -0,0 +1,38 @@
+package dv_ds_20241c_g18.tiendaRopita;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
